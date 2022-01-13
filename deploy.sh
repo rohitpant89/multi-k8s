9cc17212943537b830c159b528e617f09b3223a3
Rohit Pant <rohit.pant@comviva.com>
2022-01-14 00:06:11 +0530

    TRAVIS CI Fixed 7

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t rohitpant89/multi-client:latest -t rohitpant89/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t rohitpant89/multi-server:latest -t rohitpant89/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t rohitpant89/multi-worker:latest -t rohitpant89/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push rohitpant89/multi-client:$SHA 
-docker push rohitpant89/multi-server:$SHA
-docker push rohitpant89/multi-worker:$SHA
-docker push rohitpant89/multi-client:latest
-docker push rohitpant89/multi-server:latest
-docker push rohitpant89/multi-worker:latest
+docker build -t rohitpan89/multi-client:latest -t rohitpan89/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rohitpan89/multi-server:latest -t rohitpan89/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rohitpan89/multi-worker:latest -t rohitpan89/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push rohitpan89/multi-client:$SHA 
+docker push rohitpan89/multi-server:$SHA
+docker push rohitpan89/multi-worker:$SHA
+docker push rohitpan89/multi-client:latest
+docker push rohitpan89/multi-server:latest
+docker push rohitpan89/multi-worker:latest
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=rohitpant89/multi-server:$SHA
-kubectl set image deployments/client-deployment server=rohitpant89/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=rohitpant89/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=rohitpan89/multi-server:$SHA
+kubectl set image deployments/client-deployment server=rohitpan89/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=rohitpan89/multi-worker:$SHA
